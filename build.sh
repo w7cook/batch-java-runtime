b2921673a68aae2092173c3f6a5385d124999f63
William Cook <wcook@cs.utexas.edu>
2013-01-29 17:42:40 -0600

    exclude XML files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd src
-javac -d ../bin -cp ../lib/jackson-core-asl-1.3.2.jar `find . | fgrep .java`
+javac -d ../bin -cp ../lib/jackson-core-asl-1.3.2.jar `find . | fgrep -v XML | fgrep .java`
 cd ..
 cd test
-javac -d ../bin -cp ../bin:../lib/jackson-core-asl-1.3.2.jar `find . | fgrep .java`
+javac -d ../bin -cp ../bin:../lib/jackson-core-asl-1.3.2.jar `find . | fgrep -v XML | fgrep .java`
 cd ..
